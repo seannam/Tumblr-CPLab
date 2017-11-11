834ee617c94c063739d5c3e3c7b5927b455c2e7d
gerardrecinto <grecinto@ucsd.edu>
2017-11-11 11:47:11 -0800

    Initial Commit

---
+++ Tumblr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tumblr
+//
+//  Created by Gerard Recinto on 11/11/17.
+//  Copyright © 2017 Gerard Recinto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
